c5cee68a97e5aeb945638ea7ec614cd9b59ecced
Hero <78842166+Heropowerbrine@users.noreply.github.com>
2024-09-12 11:06:08 +0800

    Update ClientPrefs.hx

--- source/meta/data/ClientPrefs.hx
+++ source/meta/data/ClientPrefs.hx
@@ -30,6 +30,10 @@ class ClientPrefs {
 	public static var realistic:Bool = false;
 	public static var quarterbits:Bool = false;
 
+	#if android
+	public var storageType:String = "EXTERNAL_DATA";
+	#end
+
 	public static var downScroll:Bool = false;
 	public static var middleScroll:Bool = false;
 	public static var opponentStrums:Bool = true;
