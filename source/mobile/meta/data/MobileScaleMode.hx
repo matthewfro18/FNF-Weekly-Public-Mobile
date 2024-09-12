9a12ba19173fbbfc47119c7cc4ab2177af0cc5b0
Hero <78842166+Heropowerbrine@users.noreply.github.com>
2024-09-12 16:09:57 +0800

    What about now???
    At this point what's the diff between this and the other Scale Mode lol

--- source/mobile/meta/data/MobileScaleMode.hx
+++ source/mobile/meta/data/MobileScaleMode.hx
@@ -5,6 +5,8 @@ import flixel.system.scaleModes.BaseScaleMode;
 
 class MobileScaleMode extends BaseScaleMode
 {
+    @:isVar public var width(get, set):Null<Int> = null;
+    @:isVar public var height(get, set):Null<Int> = null;
     public static var allowWideScreen(default, set):Bool = true;
 
     override function updateGameSize(Width:Int, Height:Int):Void
@@ -34,6 +36,10 @@ class MobileScaleMode extends BaseScaleMode
 	    FlxG.width = width;
 	    FlxG.height = height;
 	}
+    public function resetSize() {
+		width = null;
+		height = null;
+    }
 
     override function updateGamePosition():Void
 	{
@@ -42,6 +48,23 @@ class MobileScaleMode extends BaseScaleMode
         else
             super.updateGamePosition();
 	}
+
+    private inline function get_width():Null<Int>
+	    return this.width == null ? FlxG.initialWidth : this.width;
+    private inline function get_height():Null<Int>
+	    return this.height == null ? FlxG.initialHeight : this.height;
+    private inline function set_width(v:Null<Int>):Null<Int> {
+	    this.width = v;
+	    @:privateAccess
+	    FlxG.game.onResize(null);
+	    return v;
+    }
+    private inline function set_height(v:Null<Int>):Null<Int> {
+	    this.height = v;
+	    @:privateAccess
+	    FlxG.game.onResize(null);
+	    return v;
+    }
 
     @:noCompletion
     private static function set_allowWideScreen(value:Bool):Bool
