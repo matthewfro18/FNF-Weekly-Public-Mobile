13e74c67a14e6f8b1f110259f4490dd6723a20db
Hero <78842166+Heropowerbrine@users.noreply.github.com>
2024-09-15 17:26:08 +0800

    Update MusicBeatState.hx

--- source/meta/states/MusicBeatState.hx
+++ source/meta/states/MusicBeatState.hx
@@ -55,7 +55,7 @@ class MusicBeatState extends FlxUIState
 
 	#if mobile
 	public function addVirtualPad(?DPad:FlxDPadMode, ?Action:FlxActionMode) {
-        _virtualpad = new FlxVirtualPad(DPad, Action, 0.75, ClientPrefs.globalAntialiasing);
+        _virtualpad = new FlxVirtualPad(DPad, Action);
 		add(_virtualpad);
 		controls.setVirtualPadUI(_virtualpad, DPad, Action);
 		trackedinputsUI = controls.trackedinputsUI;
