0ffa541c19d80da997292c7a64d4e95fecdec601
Hero <78842166+Heropowerbrine@users.noreply.github.com>
2024-09-17 11:41:31 +0800

    Update NoteOffsetState.hx

--- source/meta/data/options/NoteOffsetState.hx
+++ source/meta/data/options/NoteOffsetState.hx
@@ -195,6 +195,11 @@ class NoteOffsetState extends MusicBeatState
 		add(changeModeText);
 		updateMode();
 
+		#if mobile
+		addVirtualPad(LEFT_RIGHT,A_B_X);
+		addVirtualPadCamera();
+		#end
+
 		Conductor.changeBPM(128.0);
 		FlxG.sound.playMusic(Paths.music('offsetSong'), 1, true);
 
@@ -296,7 +301,7 @@ class NoteOffsetState extends MusicBeatState
 				}
 			}
 
-			if(controls.RESET)
+			if(controls.RESET #if mobile || _virtualpad.buttonX.justPressed #end)
 			{
 				for (i in 0...ClientPrefs.comboOffset.length)
 				{
@@ -307,25 +312,25 @@ class NoteOffsetState extends MusicBeatState
 		}
 		else
 		{
-			if(controls.UI_LEFT_P)
+			if(controls.UI_LEFT_P #if mobile || _virtualpad.buttonLeft.justPressed #end)
 			{
 				barPercent = Math.max(delayMin, Math.min(ClientPrefs.noteOffset - 1, delayMax));
 				updateNoteDelay();
 			}
-			else if(controls.UI_RIGHT_P)
+			else if(controls.UI_RIGHT_P #if mobile || _virtualpad.buttonRight.justPressed #end)
 			{
 				barPercent = Math.max(delayMin, Math.min(ClientPrefs.noteOffset + 1, delayMax));
 				updateNoteDelay();
 			}
 
 			var mult:Int = 1;
-			if(controls.UI_LEFT || controls.UI_RIGHT)
+			if(controls.UI_LEFT || controls.UI_RIGHT #if mobile || _virtualpad.buttonLeft.pressed || _virtualpad.buttonRight.pressed #end)
 			{
 				holdTime += elapsed;
-				if(controls.UI_LEFT) mult = -1;
+				if(controls.UI_LEFT #if mobile || _virtualpad.buttonLeft.justPressed #end) mult = -1;
 			}
 
-			if(controls.UI_LEFT_R || controls.UI_RIGHT_R) holdTime = 0;
+			if(controls.UI_LEFT_R || controls.UI_RIGHT_R #if mobile || _virtualpad.buttonLeft.justReleased || _virtualpad.buttonRight.justReleased #end) holdTime = 0;
 
 			if(holdTime > 0.5)
 			{
@@ -334,7 +339,7 @@ class NoteOffsetState extends MusicBeatState
 				updateNoteDelay();
 			}
 
-			if(controls.RESET)
+			if(controls.RESET #if mobile || _virtualpad.buttonX.justPressed #end)
 			{
 				holdTime = 0;
 				barPercent = 0;
@@ -342,13 +347,13 @@ class NoteOffsetState extends MusicBeatState
 			}
 		}
 
-		if(controls.ACCEPT)
+		if(controls.ACCEPT #if mobile || _virtualpad.buttonA.justPressed #end)
 		{
 			onComboMenu = !onComboMenu;
 			updateMode();
 		}
 
-		if(controls.BACK)
+		if(controls.BACK #if mobile || _virtualpad.buttonB.justPressed #end)
 		{
 			if(zoomTween != null) zoomTween.cancel();
 			if(beatTween != null) beatTween.cancel();
