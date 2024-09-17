d4aa24ef382c7ab7fe32822c1633462ff3e7b6ba
Hero <78842166+Heropowerbrine@users.noreply.github.com>
2024-09-17 22:40:44 +0800

    Update FreeplayState.hx

--- source/meta/states/FreeplayState.hx
+++ source/meta/states/FreeplayState.hx
@@ -207,7 +207,7 @@ class FreeplayState extends MusicBeatState
 		var leText:String = "Press SPACE to listen to the Song / Press CTRL to open the Gameplay Changers Menu / Press RESET to Reset your Score and Accuracy.";
 		var size:Int = 16;
 		#else
-		var leText:String = "Press CTRL to open the Gameplay Changers Menu / Press RESET to Reset your Score and Accuracy.";
+		var leText:String = "Press C to open the Gameplay Changers Menu / Press X to Reset your Score and Accuracy.";
 		var size:Int = 18;
 		#end
 		var text:FlxText = new FlxText(textBG.x, textBG.y + 4, FlxG.width, leText, size);
