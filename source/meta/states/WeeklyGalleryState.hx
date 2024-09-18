01acd9f74738efae26f0c4d50e07e71a2b9b443f
Hero <78842166+Heropowerbrine@users.noreply.github.com>
2024-09-18 21:39:36 +0800

    Update WeeklyGalleryState.hx

--- source/meta/states/WeeklyGalleryState.hx
+++ source/meta/states/WeeklyGalleryState.hx
@@ -240,7 +240,7 @@ class WeeklyGalleryState extends MusicBeatState
         }
         FlxObjectTools.centerOnSprite(image, bg);
 
-        description.text = weekDescriptions[curWeek][curImg];
+        description.text = CoolUtil.coolTextFile('assets/images/gallery/weeks/${weekDescriptions[curWeek][curImg]}');
         counter.text = '${curImg + 1}';
 
         FlxG.sound.play(Paths.sound('scrollMenu'));
