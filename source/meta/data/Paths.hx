b78c4de290fff6d26c41cf9fd9cdcae87e31f595
matthewfro1818 <155072293+matthewfro1818@users.noreply.github.com>
2024-09-23 06:06:25 -0400

    Update Paths.hx

--- source/meta/data/Paths.hx
+++ source/meta/data/Paths.hx
@@ -302,10 +302,8 @@ class Paths
 
 	static public function getTextFromFile(key:String, ?ignoreMods:Bool = false):String
 	{
-		#if MODS_ALLOWED
 		if (!ignoreMods && FileSystem.exists(modFolders(key)))
 			return File.getContent(modFolders(key));
-		#end
 
 		if (FileSystem.exists(getPreloadPath(key)))
 			return File.getContent(getPreloadPath(key));
